f18da1f124ed013b56976adc53248ee862aeefbf
Vincent <vincent854321@gmail.com>
2023-05-11 13:48:08 -0500

    added div for not matching passwords

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 console.log("hello world!");
-document.addEventListener("DOMContentLoaded",function(){
-    let password = document.querySelector("#password").value;
 
+const input = document.querySelector("#password");
+
+document.addEventListener("input", function () {
+    let password = document.querySelector("#password").value;
     let confirmPassword = document.querySelector("#confirmPassword").value;
 
-    if(password != confirmPassword){
-        console.log("HI");
+    let div = document.getElementById('match')
+
+    if (password != confirmPassword) {
+        div.style.display = "block";
+        password.style.border = "none";
+        confirmPassword.style.border = "none";
+
+        password.style.border = "1px solid red";
+        confirmPassword.style.border = "1px solid red";
+    } else {
+        div.style.display = "none";
     }
+
+    console.log("FIRST: " + password);
+    console.log("SECOND: " + confirmPassword);
 })
