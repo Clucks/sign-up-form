8800b9fcc6634d1a5b0ef2570ba069af1fd5f1d4
Vincent <vincent854321@gmail.com>
2023-05-11 14:54:49 -0500

    fixed all forms, added styling for button, and odin logo

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ console.log("hello world!");
 const input = document.querySelector("#password");
 
 document.addEventListener("input", function () {
-    let password = document.querySelector("#password").value;
-    let confirmPassword = document.querySelector("#confirmPassword").value;
+    let password = document.querySelector("#password");
+    let confirmPassword = document.querySelector("#confirmPassword");
 
-    let div = document.getElementById('match')
+    let div = document.getElementById('match');
 
-    if (password != confirmPassword) {
+    if (password.value != confirmPassword.value) {
         div.style.display = "block";
         password.style.border = "none";
         confirmPassword.style.border = "none";
@@ -16,6 +16,8 @@ document.addEventListener("input", function () {
         password.style.border = "1px solid red";
         confirmPassword.style.border = "1px solid red";
     } else {
+        password.style.border = "1px solid #E5E7EB";
+        confirmPassword.style.border = "1px solid #E5E7EB";
         div.style.display = "none";
     }
 
